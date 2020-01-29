5837e7ee1c0f9ce1de84abd0bee4761b4152731d
Andrew Danowitz <adanowitz@gmail.com>
2020-01-28 18:05:23 -0800

    added analysis for other anxiety and refactor parse so that similar likert maps could be shared among functions

--- rfunc/parse_funcs.r
+++ rfunc/parse_funcs.r
@@ -1,23 +1,58 @@
+parse.var.yes_no <- data.frame(likert = c("Yes", "No"),
+                     value=c(1,0),stringsAsFactors=FALSE)
+parse.var.not_sev_mohalf_ned <- data.frame(likert = c("Not at all", "Several days",
+                                    "More than half the days",
+                                    "Nearly every day"),
+                                    value=c(0,0,1,1),stringsAsFactors=FALSE)
+parse.var.not_sev_mohalf <- data.frame(likert = c("Not at all", "Several Days",
+                                                      "More than half the days"),
+                                           value=c(0,0,1),stringsAsFactors=FALSE)
+parse.phq.other_anx <- function(data){
+    lmap <- parse.var.not_sev_mohalf
+    
+    other_headers <- grep("phq_5", names(data),value=TRUE)
+    
+    data <- parse.likert(data=data, header_names = other_headers, likert_map = lmap)
+    
+    return(data)
+}
+parse.phq.panic <- function(data){
+    
+    yes_no <- parse.var.yes_no
+    panic_headers <- c(grep("phq_3", names(data),value=TRUE), grep("phq_4", names(data),value=TRUE))
+    
+    data <- parse.likert(data=data,header_names=panic_headers, likert_map=yes_no)
+    
+    return(data)
+}
+parse.phq.dep <- function(data){
+    atoh_score <- parse.var.not_sev_mohalf_ned
+    
+    i_score <- data.frame(likert = c("Not at all", "Several Days",
+                                        "More than half the days",
+                                        "Nearly every day"),
+                          value=c(0,1,1,1),stringsAsFactors=FALSE)
+
+    dep_headers <- grep("phq_2", names(data),value=TRUE)
+    
+    data <- parse.likert(data=data, header_names=dep_headers[1:8],
+                         likert_map=atoh_score)
+    data <- parse.likert(data=data, header_names=dep_headers[9],
+                         likert_map=i_score)
+    
+    return(data)
+}
 parse.kessler <- function(data){
 
     kesscore <- data.frame(likert = c("All of the time","Most of the time","Some of the time","A little of the time","None of the time"), value=c(4,3,2,1,0),stringsAsFactors = FALSE)
     #Grab the kessler 6 headings
-    kessler_headers <- kessnames <- grep("_", grep("kessler", names(cpdata), value=TRUE), invert = TRUE, value = TRUE)
+    kessler_headers <- kessnames <- grep("_", grep("kessler", names(data), value=TRUE), invert = TRUE, value = TRUE)
 
     #Copy the relevant kesler rows for manipulation
     #kesslerdata <- data[kessler_headers]
 
     data <- parse.likert(data=data, header_names=kessler_headers,
                          likert_map=kesscore)
-    #data[kessler_headers] <- kesslerdata %>%
-    #    pivot_longer(cols = kessler_headers,names_to = "kessler",
-    #                 values_to="likert") %>%
-    #    left_join(kesscore, by = "likert") %>%
-    #    mutate(likert=value) %>%
-    #    select(-value) %>%
-    #    pivot_wider(names_from = "kessler", values_from="likert",
-    #                values_fn = list(likert = list)) %>%
-    #    unnest(cols=kessler_headers)
 
     return(data)
 }
--- rfunc/phq_analysis.r
+++ rfunc/phq_analysis.r
@@ -1,11 +1,11 @@
-analysis.phq.major <- function(data){
+analysis.phq.depr <- function(data){
 
                                         #Grab phq_2a-2i
     dep_headers <- grep("phq_2", names(data),value=TRUE)
 
     #Parse the data to replace Likert style scores with 1s if they count and 0s
     #if they don't
-    data <- parse.phqdep(data)
+    data <- parse.phq.dep(data)
 
     data <- mutate(data,
                       dep_major = if_else(
@@ -24,6 +24,30 @@ analysis.phq.major <- function(data){
                        (rowSums(data[dep_headers[1:2]])>=1), 1 ,
                        dep_major)
                    )
+    
+    return(data)
+}
+
+analysis.phq.panic <- function(data){
+    data <- parse.phq.panic(data)
+    
+    panic3_headers <- grep("phq_3", names(data),value=TRUE)
+    panic4_headers <- grep("phq_4", names(data),value=TRUE)
+    
+    data <- data %>% mutate(
+        panic = if_else(rowSums(data[panic3_headers])==4 | (data[panic3_headers[1]] & rowSums(data[panic4_headers])>=4), 1, 0)
+    )
+    
+    return(data)
+}
+
+analysis.phq.other <- function(data){
+    data <- parse.phq.other_anx(data)
+    other_headers <- grep("phq_5", names(data),value=TRUE)
+    
+    data <- data %>% mutate(
+        other_anxiety = if_else(data[other_headers[1]] & rowSums(data[other_headers[-1]])>=3, 1, 0)
+    )
     
     return(data)
 }
